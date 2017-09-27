9466397d388403492c46e197f17a1346ea829180
cc <15355498770@163.com>
2017-09-28 00:38:29 +0800

    add game.py

---
+++ com.chen.demo/Demo2.py
@@ -0,0 +1,23 @@
+
+name = input("what is your name?")
+print("hello "+name+"!")
+
+
+print("adada"
+      "dasdasdas"
+      "asdasdas "
+      "----\n"
+      "dasdasdasgsda"
+      ""
+      "fsdfasfasdfa"
+      ""
+      "adf"
+      "a"
+      "dfda"
+      "s")
+
+import sys
+print(sys.getdefaultencoding())
+
+
+
---
+++ com.chen.python/Game.py
@@ -0,0 +1,11 @@
+
+temp = input("来，猜个数字吧:")
+guess = int(temp)
+if guess == 8:
+    print("the number is 8 , great! ")
+else:
+    print("you are wrong , the number is 8 ")
+
+print("game over ~ ")
+
+
